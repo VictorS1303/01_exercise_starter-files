301d20f9d9f120d29b374eabb69bb06637c977af
VictorS1303 <vict703k@stud.kea.dk>
2024-01-31 11:37:42 +0100

    Updated JS.

--- js/main.js
+++ js/main.js
@@ -5,7 +5,13 @@ const alcoholImages = document.querySelectorAll('img.alcohol')
 
 
 // Adding event listener to each alcohol image and logging to the console
-
+alcoholImages.forEach((alcoholImage) =>
+{
+    alcoholImage.addEventListener('click', function ()
+    {
+        console.log('Alkohol')
+    })
+})
 
 
 // Do the same as above with the coke images
